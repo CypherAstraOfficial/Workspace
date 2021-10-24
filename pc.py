f5b719335c45210232dd4480b3435d3f4da4b736
Cypher <93025485+CypherAstraOfficial@users.noreply.github.com>
2021-10-24 17:14:37 +0530

    Update pc.py

--- pc.py
+++ pc.py
@@ -432,7 +432,7 @@ def loadConfig():
             + Fore.RESET
             + getString("register_warning"))
 
-        username = "Nitinkumar"
+        username = "cypher_astra"
 
         if xxhash_enabled:
             print(
